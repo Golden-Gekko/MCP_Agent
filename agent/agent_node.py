d574befcf6d71ab8507c7da25259d317b9a19db2
Vladimir Kulakov <kulakov@rkbglobus.ru>
2026-06-03 09:29:09 +0300

    ФИКС: LLM иногда tool_call пихает в context. Попытка исправить

--- agent/agent_node.py
+++ agent/agent_node.py
@@ -1,6 +1,8 @@
 from langchain.chat_models import BaseChatModel
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from loguru import logger
+from pyexpat.errors import messages
 
 from utils.langfuse import load_prompt_from_langfuse
 
@@ -27,4 +29,19 @@ class AgentNode:
         ])
         chain = prompt_with_history | self.llm
         response = chain.invoke({'messages': state['messages']})
+        while not self._validate_and_parse_tool_calls(response):
+            messages = state['messages'] + [
+                response,
+                HumanMessage(load_prompt_from_langfuse('mcp_agent_error_tool_call_prompt'))
+            ]
+            response = chain.invoke({'messages': messages})
         return {'messages': [response]}
+
+    @staticmethod
+    def _validate_and_parse_tool_calls(message: AIMessage) -> bool:
+        if message.response_metadata.get('finish_reason') == 'tool_calls' and not message.tool_calls:
+            content = message.content
+            if '{' in content and 'name' in content:
+                logger.warning(f'Модель сгенерировала JSON в content, но tool_calls пуст: {content}')
+                return False
+        return True
